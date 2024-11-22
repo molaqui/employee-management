70f7ff0c2e439bd04c24f033c798803b88e97649
molaqui <abdrahimmolaqui@gmail.com>
2024-11-22 17:59:24 +0100

    add back-end

--- employee-management-backend/main.go
+++ employee-management-backend/main.go
@@ -14,10 +14,10 @@ func main() {
 	// Connecte à MongoDB
 	config.ConnectDB()
 
-	// Initialiser Gin
+	
 	router := gin.Default()
 
-	// Middleware CORS
+
 	router.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
@@ -29,10 +29,9 @@ func main() {
 		c.Next()
 	})
 
-	// Définir les routes
+	
 	routes.EmployeeRoutes(router)
 
-	// Lancer le serveur
 	log.Println("Serveur lancé sur http://localhost:8080")
 	router.Run(":8080")
 }
